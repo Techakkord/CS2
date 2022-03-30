d1f9b4bfe22e1ed0955eabec1ee5e3559599c653
Techakkord <jonakkord@mail.ru>
2022-03-31 03:02:43 +0600

    Add project files.

---
+++ Asteroids/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Asteroids
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
